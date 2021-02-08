bb75996edd2f62e78e7b6d2bceb9752438d99384
vitorhugoprado <58527338+vitorhugoprado@users.noreply.github.com>
2021-02-08 18:44:44 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -13,11 +13,12 @@ int mul(int a, int b);
 
 int add(int a, int b)
 {
+   a+b;
    return 0; // TODO: fix
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
-   return 0; // TODO: fix
+   return a*b; // TODO: fix
 }
